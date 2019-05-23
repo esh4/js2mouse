27c366ac554b47e72875d90bf34214972c6e57aa
eshel <eshel.sinclair@gmail.com>
2019-05-23 19:27:18 +0300

    added scroll thread

--- main.py
+++ main.py
@@ -15,18 +15,20 @@ js = Joystick(0)
 
 mouse = pymouse.PyMouse()
 MOUSE_MOVE_MULTIPLIER: int = 30
-SCROLL_MULTIPLIER: int = 30
+SCROLL_MULTIPLIER: int = 10
 
 
 btn_queue = Queue()
 
 # js.set_button_clicked_callback(0, lambda: btn_queue.put((lambda m: m.click(m.position()[0], m.position()[1]), mouse)))
-js.set_button_pressed_callback(0, lambda: btn_queue.put((lambda m: m.press(m.position()[0], m.position()[1], button=1), mouse)))
-js.set_button_released_callback(0, lambda: btn_queue.put((lambda m: m.release(m.position()[0], m.position()[1], button=1), mouse)))
+js.set_button_pressed_callback(3, lambda: btn_queue.put((lambda m: m.press(m.position()[0], m.position()[1], button=1), mouse)))
+js.set_button_released_callback(3, lambda: btn_queue.put((lambda m: m.release(m.position()[0], m.position()[1], button=1), mouse)))
 
-js.set_button_pressed_callback(1, lambda: btn_queue.put((lambda m: m.press(m.position()[0], m.position()[1], button=2), mouse)))
-js.set_button_released_callback(1, lambda: btn_queue.put((lambda m: m.release(m.position()[0], m.position()[1], button=2), mouse)))
+js.set_button_pressed_callback(0, lambda: btn_queue.put((lambda m: m.press(m.position()[0], m.position()[1], button=2), mouse)))
+js.set_button_released_callback(0, lambda: btn_queue.put((lambda m: m.release(m.position()[0], m.position()[1], button=2), mouse)))
 
+js.set_button_pressed_callback(12, lambda: btn_queue.put((lambda m: m.press(m.position()[0], m.position()[1], button=3), mouse)))
+js.set_button_released_callback(12, lambda: btn_queue.put((lambda m: m.release(m.position()[0], m.position()[1], button=3), mouse)))
 
 def handle_btns():
     while True:
@@ -37,14 +39,21 @@ def handle_btns():
         time.sleep(0.04)
 
 
+def scroll(mouse, movement_callback):
+    while True:
+        mouse.scroll(vertical=movement_callback())
+        time.sleep(0.2)
+
+
 btn_thread = Thread(target=handle_btns)
 btn_thread.setDaemon(True)
 btn_thread.setName('btn_thread')
 btn_thread.start()
 
-# for id, btn in js.buttons.items():
-#     print('{} pressed:   '.format(id), btn.on_pressed)
-
+scroll_thread = Thread(target=scroll, args=(mouse, lambda: -js.get_axis(4)*SCROLL_MULTIPLIER))
+scroll_thread.setName('scroll_thread')
+scroll_thread.setDaemon(True)
+scroll_thread.start()
 
 while True:
     pygame.event.pump()
@@ -56,9 +65,6 @@ while True:
     mov_x = int(js.get_axis(0) * MOUSE_MOVE_MULTIPLIER)
     mov_y = int(js.get_axis(1) * MOUSE_MOVE_MULTIPLIER)
     mouse.move(current_pos[0] + mov_x, current_pos[1] + mov_y)
-
-    # scroll
-    mouse.scroll(vertical=-js.get_axis(4)*SCROLL_MULTIPLIER, horizontal=js.get_axis(5))
 
     time.sleep(0.03)
 
